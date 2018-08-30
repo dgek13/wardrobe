4e55eeb6fef2f43d7e072d4e22e144975527fc12
Evgeniy I. Akimov <dgek13@gmail.com>
2018-08-30 22:23:24 +0300

    correct programm4

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ api_key = "e809fbd81e2a508842b45ad0a5c8bbc9"
 spreadsheet_key = "1bPT458nCSWahOXu6FcRmv3ASWC1Yl0UuyiTlTRCBUAI"
 city = "Moscow"
 telegram_bot_api_key =
-  "650565763:AAHX_0_8l3TFgfRr1UnoOOFTKOPlVEb3ekE"
+  "635203088:AAGO0ScwfLi2jETzIJ9vsFPbbSN8qm3xSkg"
 
 Telegram::Bot::Client.run(telegram_bot_api_key) do |bot|
   bot.listen do |message|
