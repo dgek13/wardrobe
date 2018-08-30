17d0a375b4373d44cc871c8c9a299d4b3ff05498
Evgeniy I. Akimov <dgek13@gmail.com>
2018-08-30 21:17:59 +0300

    Gemfile + test telegram bot added

---
+++ test.rb
@@ -0,0 +1,14 @@
+require 'telegram/bot'
+
+token = '635203088:AAGO0ScwfLi2jETzIJ9vsFPbbSN8qm3xSkg'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
