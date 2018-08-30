e461ec80da7f78da34d5f82ea586e31f05dda61f
Evgeniy I. Akimov <dgek13@gmail.com>
2018-08-30 22:19:51 +0300

    correct programm2

--- main.rb
+++ main.rb
@@ -1,46 +1,38 @@
 require "json"
 require "net/http"
+require "telegram/bot"
 
 require_relative "lib/clothes_item"
 require_relative "lib/wardrobe"
 require_relative "lib/open_weather_map"
-require_relative ""
 
-# конфигурация
-city = "Moscow"
 api_key = "e809fbd81e2a508842b45ad0a5c8bbc9"
-sereadsheet_key = "1bPT458nCSWahOXu6FcRmv3ASWC1Yl0UuyiTlTRCBUAI"
-
-#------------
-
+spreadsheet_key = "1bPT458nCSWahOXu6FcRmv3ASWC1Yl0UuyiTlTRCBUAI"
+city = "Moscow"
+telegram_bot_api_key =
+  "650565763:AAHX_0_8l3TFgfRr1UnoOOFTKOPlVEb3ekE"
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Привет, #{message.from.first_name}!")
     when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Пока, #{message.from.first_name}!")
     else
-    temperature = open_weather_current_temperature
+      temperature = open_weather_current_temperature
 
-    temperature_text = "По данным openweathermap.org в Москве сейчас #{temperature}°C"
+      temperature_text = "По данным openweathermap.org в Москве сейчас #{temperature}°C"
 
-    wardrobe = Wardrobe.new(sereadsheet_key)
+      wardrobe = Wardrobe.new(spreadsheet_key)
 
-    items = wardrobe.random_suitable_items(temperature)
+      items = wardrobe.random_suitable_items(temperature)
 
       items_text = "Предлагая сегодня надеть:\n\n"
-      # a = "foo"
-      # a += "bar"
-      # => "foobar"
-      # a = a + "bar"
       items_text += items.map { |item| "#{item.info}\n" }
 
-    bot.api.send_message(chat_id: message.chat.id, text: temperature_text)
-    bot.api.send_message(chat_id: message.chat.id, text: items_text)
-
+      bot.api.send_message(chat_id: message.chat.id, text: temperature_text)
+      bot.api.send_message(chat_id: message.chat.id, text: items_text)
     end
   end
-end
 end
